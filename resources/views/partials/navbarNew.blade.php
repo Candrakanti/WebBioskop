76807c710e80233398bba465e88369370274d649
Candrakanti <candrakantid26@gmail.com>
2022-09-14 11:36:59 +0700

    changes

---
+++ resources/views/partials/navbarNew.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid">
+        <img class="label ms" src="img/SeeNemass.png" alt="" width="150" height="150" style="margin-left: 200px;" >
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mb-3">
+
+          <li class="nav-item ">
+            <a class="nav-link" href="#">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MOVIE</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">TICKET</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MYCGV</a>
+          </li>
+
+        </ul>
+        <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link " href="/login">LOGIN</a>
+        </li>
+        </ul>
+    </div>
+    </div>
+  </nav>
+
+  <div class="container mb-5">
+    <hr class="border border-danger border-2 opacity-50">
+</div>
+
