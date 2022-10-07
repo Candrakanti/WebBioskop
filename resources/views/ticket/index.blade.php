73c11d03c6c52a9f7bd220648a83b190ed36b23c
Amandaps23 <amandapuspita896@gmail.com>
2022-10-07 08:16:01 +0700

    tiketblmfiks

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -2,4 +2,20 @@
 
 @section('container')
 
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title> Contoh Penggunaan Properti Background-Image </title>
+       
+        </head>
+
+        <body>
+        <div class="container">
+            <img src="{{ asset('img/seenema.png') }}" alt="">
+        </div>
+        </body>
+
+</html>
+
 @endsection
